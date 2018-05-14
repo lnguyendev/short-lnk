1b929f72a46441fc0158a05cae416de7ac8eed09
Lam Nguyen <lnguyendev@gmail.com>
2018-05-13 20:35:30 -0500

    Initial commit

---
+++ imports/ui/Login.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router';
+import { Meteor } from 'meteor/meteor';
+
+export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: ''
+        };
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        let email = this.refs.email.value.trim();
+        let password = this.refs.password.value.trim();
+
+        Meteor.loginWithPassword({email}, password, (error) => {
+            if (error) {
+                this.setState({
+                    error: 'Unable to login. Check email and password.'
+                });
+            } else {
+                this.setState({
+                    error: ''
+                });
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className="boxed-view">
+                <div className="boxed-view-box">
+                    <h1>Short Link</h1>
+                    {this.state.error && <p>{this.state.error}</p>}
+                    <form onSubmit={this.onSubmit.bind(this)} noValidate className="boxed-view-form">
+                        <input type="email" ref="email" name="email" placeholder="Email" />
+                        <input type="password" ref="password" name="password" placeholder="Password" />
+                        <button className="button">Login</button>
+                    </form>
+                    <Link to="/signup">Create an account</Link>
+                </div>
+            </div>
+        );
+    }
+};
